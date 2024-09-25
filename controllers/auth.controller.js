3e79d7191c35e73d3ddbc909de9267fb3f58710c
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-09-25 12:01:07 +0600

    secure true

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -54,7 +54,7 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            // secure:true,
+            secure: true,
             maxAge: age,
         }).status(200).json(userInfo);
     } catch (err) {
